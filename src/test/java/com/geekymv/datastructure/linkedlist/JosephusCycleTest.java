f7d7abddd1cc3fac63aa66380657ecdd679b4122
geekymv <ym2011678@foxmail.com>
2020-10-19 11:30:35 +0800

    约瑟夫环问题

---
+++ src/test/java/com/geekymv/datastructure/linkedlist/JosephusCycleTest.java
@@ -0,0 +1,13 @@
+package com.geekymv.datastructure.linkedlist;
+
+import org.junit.Test;
+
+public class JosephusCycleTest {
+
+    @Test
+    public void test() {
+        JosephusCycle list = new JosephusCycle();
+        list.josephus(5, 1, 2);
+    }
+
+}
