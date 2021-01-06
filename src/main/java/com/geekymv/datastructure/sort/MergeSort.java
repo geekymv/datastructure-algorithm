667e420e6f23d25b9c94aed311db73c82e6938bc
geekymv <ym2011678@foxmail.com>
2021-01-06 16:48:09 +0800

    修改变量名

--- src/main/java/com/geekymv/datastructure/sort/MergeSort.java
+++ src/main/java/com/geekymv/datastructure/sort/MergeSort.java
@@ -2,7 +2,7 @@ package com.geekymv.datastructure.sort;
 
 
 /**
- * arr[p, r] = arr[p, q] + arr[q+1, r]
+ * arr[start, end] = arr[start, mid] + arr[mid+1, end]
  *
  */
 public class MergeSort {
@@ -14,39 +14,39 @@ public class MergeSort {
     }
 
 
-    private void mergeSort(int[] arr, int p, int r) {
+    private void mergeSort(int[] arr, int start, int end) {
         // 递归终止条件
-        if(p >= r) {
+        if(start >= end) {
             return;
         }
 
         // 取p、r 中间的位置
-        int q = (p + r) / 2;
+        int q = (start + end) / 2;
 
         // 分治递归
-        mergeSort(arr, p, q);
-        mergeSort(arr, q+1, r);
+        mergeSort(arr, start, q);
+        mergeSort(arr, q+1, end);
 
         // 合并数据
-        merge(arr, p, q, r);
+        merge(arr, start, q, end);
     }
 
 
     /**
-     * arr[p, r] = arr[p, q] + arr[q+1, r]
+     * arr[start, end] = arr[start, mid] + arr[mid+1, end]
      * @param arr
-     * @param p 第一个数组的起始下标
-     * @param q 第一个数组的终止下标
-     * @param r 第二个数组的终止下标
+     * @param start 左边数组的起始下标
+     * @param mid 左边数组的终止下标
+     * @param end 右边数组的终止下标
      */
-    public void merge(int arr[], int p, int q, int r) {
-        int[] temp = new int[r-p+1]; // 申请一个和arr[p, r] 长度一样的数组
+    public void merge(int arr[], int start, int mid, int end) {
+        int[] temp = new int[end-start+1]; // 申请一个和arr[p, r] 长度一样的数组
         int k = 0; // 指向临时数组的起始位置
-        int i = p; // 指向第一个数组的起始位置
-        int j = q+1; // 指向第二个数组的起始位置
+        int i = start; // 指向左边数组的起始位置
+        int j = mid+1; // 指向右边数组的起始位置
 
-        // 把 arr[p, q] 和 arr[q+1, r] 中较小的数添加到temp中
-        while(i <=q && j <= r) {
+        // 把 arr[start, mid] 和 arr[mid+1, end] 中较小的数添加到temp中
+        while(i <=mid && j <= end) {
             if(arr[i] <= arr[j]) {
                 temp[k++] = arr[i++];
             }else {
@@ -55,17 +55,17 @@ public class MergeSort {
         }
 
         // 将左边剩余的数据拷贝到temp
-        while (i <= q) {
+        while (i <= mid) {
             temp[k++] = arr[i++];
         }
         // 将右边剩余的数据拷贝到temp
-        while (j <= r) {
+        while (j <= end) {
             temp[k++] = arr[j++];
         }
 
         // 将temp 数组中的数据拷贝到arr
         for(int m = 0, len = temp.length; m < len; m++) {
-            arr[p+m] = temp[m];
+            arr[start+m] = temp[m];
         }
     }
 
